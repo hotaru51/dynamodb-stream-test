f304c45d9e3937d50643c05fac872643552a06f3
hotaru <36354847+hotaru51@users.noreply.github.com>
2023-07-09 16:28:57 +0900

    リクエスト受信用のLambda関数追加

--- lib/dynamodb-streams-test-stack.ts
+++ lib/dynamodb-streams-test-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class DynamodbStreamsTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DynamodbStreamsTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const receiveFunction = new lambda.Function(this, 'ReceiveFunction', {
+      runtime: lambda.Runtime.PYTHON_3_10,
+      handler: 'app.handler',
+      architecture: lambda.Architecture.X86_64,
+      code: lambda.Code.fromAsset('src/receive_function', {
+        bundling: {
+          image: lambda.Runtime.PYTHON_3_10.bundlingImage,
+          command: [
+            'bash', '-c',
+            'pip install -r requirements.txt -t /asset-output && cp -au . /asset-output'
+          ]
+        }
+      }),
+      functionName: 'dynamodb-streams-test-receive-function',
+      memorySize: 128
+    })
   }
 }
---
+++ src/receive_function/app.py
@@ -0,0 +1,16 @@
+import json
+import logging
+
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def handler(event, context):
+    logger.info('receive event: {}'.format(json.dumps(event)))
+
+    return {
+            "statusCode": 200,
+            "headers": "application/json",
+            "body": json.dumps({"msg": "ok"})}
